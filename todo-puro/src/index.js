59c30fc631780cef3d25e1f6f20da06f9c988b56
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-09-20 21:21:11 -0300

    feat: estilizando cards

---
+++ todo-puro/src/index.js
@@ -0,0 +1,8 @@
+let form = document.getElementById('form')
+let input = document.getElementById('task')
+
+const list = document.getElementById('list')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
