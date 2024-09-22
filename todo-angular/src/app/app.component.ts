0d19875e153333ff73b97fc326f8176da20a92ca
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-09-22 09:52:07 -0300

    feat: estilizando a pagina

--- todo-angular/src/app/app.component.ts
+++ todo-angular/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatInputModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
